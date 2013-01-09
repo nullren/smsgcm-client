7a3b69e29da44b542d758653ad7af3560be89a09
nullren <chopflop@gmail.com>
2013-01-08 21:23:27 -0800

    added SettingsActivity

---
+++ src/com/omgren/apps/smsgcm/client/SettingsActivity.java
@@ -0,0 +1,12 @@
+package com.omgren.apps.smsgcm.client;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.layout.settings);
+    }
+}
