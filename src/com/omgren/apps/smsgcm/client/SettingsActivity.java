d6ccf52c305a8aabb244c71c191123c7a5de0897
nullren <chopflop@gmail.com>
2013-01-09 13:27:12 -0800

    settings activity nonsense from eclipse

--- src/com/omgren/apps/smsgcm/client/SettingsActivity.java
+++ src/com/omgren/apps/smsgcm/client/SettingsActivity.java
@@ -7,6 +7,7 @@ public class SettingsActivity extends PreferenceActivity {
     public static final String PREFS_NAME = "SettingsFile";
     public static final String PREF_CERT_PASSWORD = "pref_cert_password";
 
+    @SuppressWarnings("deprecation")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
